bc67acf116a44757108f2de283aba904349d6c41
brilubel15 <a01022972@itesm.mx>
2020-09-15 11:26:40 -0500

    Data

--- data/io_utilities.py
+++ data/io_utilities.py
@@ -1,1 +1,11 @@
-class Student()
+# io_utilities.py
+
+filepath = './data/iris.data'
+
+with open(filepath,'r') as fp:
+    data = fp.read()
+
+data_lines = data.split('\n')
+data_final = [f.split(',')for f in data_lines]
+
+print(data_final)
